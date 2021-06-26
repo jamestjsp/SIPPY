a3f01d693e4eed7abf87fc29e49382a2805555fa
jamestjsp <53532429+jamestjsp@users.noreply.github.com>
2021-06-26 17:08:41 +0530

    commented python requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 	long_description_content_type='text/markdown',
 	url="https://github.com/CPCLAB-UNIPI/sippy",
 	packages=setuptools.find_packages(),
-	python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<=3.7.*,<=3.8.*,!=3.9.*", 
+	#python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<=3.7.*,<=3.8.*,!=3.9.*", 
 
 	install_requires=(
 		"numpy",
